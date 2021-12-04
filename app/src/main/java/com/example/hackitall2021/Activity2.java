23b35427cb6ab6967f0987daacfd969b1cfa37f0
stefan1317 <stefancostin.manta@yahoo.com>
2021-12-04 18:31:45 +0200

    comit4

---
+++ app/src/main/java/com/example/hackitall2021/Activity2.java
@@ -0,0 +1,15 @@
+package com.example.hackitall2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
